29316931cbf4f8f7e8cb63b7f7314772ef6630a9
Imtanan Raja <imtanan.raja@hotmail.com>
2018-06-20 09:56:40 +0100

    Star-game
    update css and lodash library for number manipulation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+var _ = require("lodash");
 import "./styles.css";
 
-function App() {
+const Stars = props => {
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="col-5">
+      {_.range(numberOfStars).map(i => <i key={i} className="fa fa-star" />)}
     </div>
   );
+};
+
+const Button = props => {
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
+};
+
+const Answer = props => {
+  return <div className="col-5">...</div>;
+};
+
+const Number = props => {
+  return (
+    <div className="card text-center">
+      <div>
+        {Number.list.map((number, i) => <span key={i}>{number}</span>)}
+      </div>
+    </div>
+  );
+};
+
+Number.list = _.range(1, 10);
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Number />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
